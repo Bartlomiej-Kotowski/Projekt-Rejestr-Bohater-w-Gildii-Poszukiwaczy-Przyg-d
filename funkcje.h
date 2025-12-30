db11203023f2ac60c87e7b3569acf3a9333d29dd
DESKTOP-3HL8OL4\kotek <95285@student.pb.edu.pl>
2025-12-30 15:36:42 +0100

    pierwsza wersja funkcji

---
+++ funkcje.h
@@ -0,0 +1,12 @@
+#ifndef FUNKCJE_H
+#define FUNKCJE_H
+
+bohater* dadaj_bohatera(bohater *głowa);
+bohater* usun_bohatera(bohater *głowa);
+void edytuj_bohatera(bohater *głowa);
+void wyszukaj_bohatera(bohater *głowa);
+void sortuj_bohatera(bohater *głowa);
+void wyswietl_liste(bohater *głowa);
+void zwolnij_liste(bohater *głowa);
+
+#endif
